ef6f91ccdca4948f17698c5a850548604bf73404
Steady Kambodji <steady.kambodji@astra-land.co.id>
2024-05-08 18:12:18 +0700

    Create & update  note

--- lib/pages/note.dart
+++ lib/pages/note.dart
@@ -29,47 +29,35 @@ class _NoteScreenState extends State<NoteScreen> {
   TextEditingController titleController = TextEditingController();
   FleatherController contentController = FleatherController();
 
-  void handleSaveTitle(String value) {
+  void handleSaveNote() {
     final realm = Realm(config);
     const duration = Duration(milliseconds: 750);
     _debouncer.debounce(
       duration: duration,
       onDebounce: () {
-        var currentNote = Note(ObjectId(), value, createdAt: DateTime.now());
-        if (_currentNote != null) {
-          currentNote =
-              Note(_currentNote!.id, value, updatedAt: DateTime.now());
+        final content = jsonEncode(contentController.document);
+        final title = titleController.text;
+        if (title.isNotEmpty) {
+          var note = Note(ObjectId(), title,
+              content: content, createdAt: DateTime.now());
+          if (_currentNote != null) {
+            note.id = _currentNote!.id;
+            note.updatedAt = DateTime.now();
+          }
+          if (_currentNote!.title != note.title ||
+              _currentNote!.content != note.content) {
+            realm.write(
+              () {
+                realm.add<Note>(note, update: true);
+                if (mounted) {
+                  setState(() {
+                    _currentNote = note;
+                  });
+                }
+              },
+            );
+          }
         }
-        realm.write(
-          () {
-            realm.add<Note>(currentNote, update: true);
-            setState(() {
-              _currentNote = currentNote;
-            });
-          },
-        );
-      },
-    );
-  }
-
-  void handleSaveContent() {
-    const duration = Duration(milliseconds: 750);
-    _debouncer.debounce(
-      duration: duration,
-      onDebounce: () {
-        /* var currentNote = Note(ObjectId(), value, createdAt: DateTime.now());
-        if (_currentNote != null) {
-          currentNote =
-              Note(_currentNote!.id, value, updatedAt: DateTime.now());
-        }
-        realm.write(
-          () {
-            realm.add<Note>(currentNote, update: true);
-            setState(() {
-              _currentNote = currentNote;
-            });
-          },
-        ); */
       },
     );
   }
@@ -78,19 +66,28 @@ class _NoteScreenState extends State<NoteScreen> {
     if (widget.existingNote != null) {
       setState(() {
         _currentNote = widget.existingNote!;
+        final document = loadDocument(_currentNote!.content!);
+        contentController = FleatherController(document: document);
       });
     }
+  }
+
+  ParchmentDocument loadDocument(String document) {
+    return ParchmentDocument.fromJson(jsonDecode(document));
   }
 
   @override
   void initState() {
     super.initState();
     getCurrentNote();
+    titleController.addListener(handleSaveNote);
+    contentController.addListener(handleSaveNote);
   }
 
   @override
   void dispose() {
     titleController.dispose();
+    contentController.dispose();
     super.dispose();
   }
 
@@ -134,7 +131,6 @@ class _NoteScreenState extends State<NoteScreen> {
               ),
               child: TextField(
                 controller: titleController,
-                onChanged: handleSaveTitle,
                 decoration: const InputDecoration(
                     hintText: "Title goes here", border: InputBorder.none),
               ),
@@ -159,85 +155,116 @@ class _NoteScreenState extends State<NoteScreen> {
                 padding: const EdgeInsets.fromLTRB(16, 6, 16, 16),
               ),
             ),
-            Container(
-                padding: const EdgeInsets.all(12),
-                margin: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: const Border(
-                    top: BorderSide(width: 2.0, color: Colors.black),
-                    left: BorderSide(width: 2.0, color: Colors.black),
-                    right: BorderSide(width: 2.0, color: Colors.black),
-                    bottom: BorderSide(width: 4.0, color: Colors.black),
-                  ),
-                  color: HexColor("#6eb9ff"),
-                ),
-                child: Wrap(
-                  alignment: WrapAlignment.spaceEvenly,
-                  children: [
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.bold,
-                      icon: FontAwesomeIcons.bold,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.italic,
-                      icon: FontAwesomeIcons.italic,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.underline,
-                      icon: FontAwesomeIcons.underline,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.strikethrough,
-                      icon: FontAwesomeIcons.strikethrough,
-                      controller: contentController,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 6, right: 6),
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.left,
-                      icon: FontAwesomeIcons.alignLeft,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.right,
-                      icon: FontAwesomeIcons.alignRight,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.center,
-                      icon: FontAwesomeIcons.alignCenter,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.justify,
-                      icon: FontAwesomeIcons.alignJustify,
-                      controller: contentController,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 6, right: 6),
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.ol,
-                      icon: FontAwesomeIcons.listOl,
-                      controller: contentController,
-                    ),
-                    ToggleStyleButton(
-                      attribute: ParchmentAttribute.ul,
-                      icon: FontAwesomeIcons.listUl,
-                      controller: contentController,
-                    ),
-                  ],
-                )),
+            editorToolbar(),
           ],
         ));
   }
 
-  Drawer endDrawer() {
+  Container editorToolbar() {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: const Border(
+          top: BorderSide(width: 2.0, color: Colors.black),
+          left: BorderSide(width: 2.0, color: Colors.black),
+          right: BorderSide(width: 2.0, color: Colors.black),
+          bottom: BorderSide(width: 4.0, color: Colors.black),
+        ),
+        color: HexColor("#6eb9ff"),
+      ),
+      child: Wrap(
+        alignment: WrapAlignment.spaceEvenly,
+        children: [
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.bold,
+            icon: FontAwesomeIcons.bold,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.italic,
+            icon: FontAwesomeIcons.italic,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.underline,
+            icon: FontAwesomeIcons.underline,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.strikethrough,
+            icon: FontAwesomeIcons.strikethrough,
+            controller: contentController,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 6, right: 6),
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.left,
+            icon: FontAwesomeIcons.alignLeft,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.right,
+            icon: FontAwesomeIcons.alignRight,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.center,
+            icon: FontAwesomeIcons.alignCenter,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.justify,
+            icon: FontAwesomeIcons.alignJustify,
+            controller: contentController,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 6, right: 6),
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.ol,
+            icon: FontAwesomeIcons.listOl,
+            controller: contentController,
+          ),
+          ToggleStyleButton(
+            attribute: ParchmentAttribute.ul,
+            icon: FontAwesomeIcons.listUl,
+            controller: contentController,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Dialog deleteDialog() {
+    return Dialog(
+      shape: const Border(
+        top: BorderSide(width: 2.0, color: Colors.black),
+        left: BorderSide(width: 2.0, color: Colors.black),
+        right: BorderSide(width: 2.0, color: Colors.black),
+        bottom: BorderSide(width: 4.0, color: Colors.black),
+      ),
+      backgroundColor: Colors.white,
+      child: Column(
+        children: [
+          Text("Are you sure you want to delete this note?"),
+          Row(
+            children: [
+              StyledButton(
+                buttonColor: Colors.red,
+                text: "Delete",
+                onPressed: () {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Drawer endDrawer(void deleteNoteFunction) {
     return Drawer(
       shape: const ContinuousRectangleBorder(
           side: BorderSide(width: 4.0, color: Colors.black)),
@@ -290,7 +317,28 @@ class _NoteScreenState extends State<NoteScreen> {
             ),
             const Padding(padding: EdgeInsets.all(12)),
             ListTile(
-              onTap: () {},
+              onTap: () {
+                _scaffoldKey.currentState!.closeEndDrawer();
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text("Cancel")),
+                          StyledButton(
+                              buttonColor: Colors.red,
+                              onPressed: () {},
+                              text: "Delete"),
+                        ],
+                        content: const Text(
+                            "Are you sure you want to delete this note?"),
+                      );
+                    });
+              },
               title: const Text("Delete note"),
             ),
           ],
